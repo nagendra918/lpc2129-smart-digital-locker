16e754e3b87bbfd5404d6d2f3c7a5665f0dde5cf
Nagendrababu <nagendrababuvadranapu@gmail.com>
2026-08-19 12:22:30 +0530

    Add hardware pin configuration

---
+++ config/pin_configuration.h
@@ -0,0 +1,33 @@
+//pin_configuration
+
+/* LED */
+#define LED_PORT      1
+#define LED_RED       19
+#define LED_GREEN     20
+#define LED_YELLOW    21
+#define LED_BLUE      22
+
+/* BUZZER */
+#define BUZZER_PORT   1
+#define BUZZER        16
+
+/* SWITCH */
+#define SWITCH_PORT   1
+#define SWITCH        17
+ 
+/* 7 SEGMENT DISPLAY */
+#define _7_SEGMENT_PORT    1
+#define _7_SEGMENT_DATA    18
+
+/* LCD */
+#define LCD_PORT       0
+#define LCD_RS         5 
+#define LCD_RW         6
+#define LCD_EN         7
+#define LCD_DATA       8 // 8 - 15
+
+/* KPM */
+#define KEYPAD_PORT    0 
+#define KEYPAD_ROWS    16
+#define KEYPAD_COLS    20
+
